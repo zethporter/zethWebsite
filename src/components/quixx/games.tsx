b4c6f737b5504c473aeab12fbd366ce2dbaee252
Zeth <zeth@bmisw.com>
2023-01-02 20:14:38 -0700

    initial commit

---
+++ src/components/quixx/games.tsx
@@ -0,0 +1,399 @@
+import { RocketLaunchIcon, LockOpenIcon, LockClosedIcon } from '@heroicons/react/24/solid';
+
+const Lock = () => {
+    return(
+        <LockOpenIcon />
+    )
+}
+
+const main = 
+    [
+        {
+            color: 'red',
+            num: 2,
+            prev: null,
+            id: 1,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'red',
+            num: 3,
+            prev: 1,
+            id: 2,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'red',
+            num: 4,
+            prev: 2,
+            id: 3,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'red',
+            num: 5,
+            prev: 3,
+            id: 4,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'red',
+            num: 6,
+            prev: 4,
+            id: 5,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'red',
+            num: 7,
+            prev: 5,
+            id: 6,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'red',
+            num: 8,
+            prev: 6,
+            id: 7,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'red',
+            num: 9,
+            prev: 7,
+            id: 8,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'red',
+            num: 10,
+            prev: 8,
+            id: 9,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'red',
+            num: 11,
+            prev: 9,
+            id: 10,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'red',
+            num: 12,
+            prev: 10,
+            id: 11,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'red',
+            num: <Lock />,
+            prev: 11,
+            id: 12,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'yellow',
+            num: 2,
+            prev: null,
+            id: 13,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'yellow',
+            num: 3,
+            prev: 13,
+            id: 14,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'yellow',
+            num: 4,
+            prev: 14,
+            id: 15,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'yellow',
+            num: 5,
+            prev: 15,
+            id: 16,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'yellow',
+            num: 6,
+            prev: 16,
+            id: 17,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'yellow',
+            num: 7,
+            prev: 17,
+            id: 18,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'yellow',
+            num: 8,
+            prev: 18,
+            id: 19,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'yellow',
+            num: 9,
+            prev: 19,
+            id: 20,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'yellow',
+            num: 10,
+            prev: 20,
+            id: 21,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'yellow',
+            num: 11,
+            prev: 21,
+            id: 22,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'yellow',
+            num: 12,
+            prev: 22,
+            id: 23,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'yellow',
+            num: <Lock />,
+            prev: 23,
+            id: 24,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'green',
+            num: 12,
+            prev: null,
+            id: 25,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'green',
+            num: 11,
+            prev: 25,
+            id: 26,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'green',
+            num: 10,
+            prev: 26,
+            id: 27,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'green',
+            num: 9,
+            prev: 27,
+            id: 28,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'green',
+            num: 8,
+            prev: 28,
+            id: 29,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'green',
+            num: 7,
+            prev: 29,
+            id: 30,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'green',
+            num: 6,
+            prev: 30,
+            id: 31,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'green',
+            num: 5,
+            prev: 31,
+            id: 32,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'green',
+            num: 4,
+            prev: 32,
+            id: 33,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'green',
+            num: 3,
+            prev: 33,
+            id: 34,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'green',
+            num: 2,
+            prev: 34,
+            id: 35,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'green',
+            num: <Lock />,
+            prev: 35,
+            id: 36,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'blue',
+            num: 12,
+            prev: null,
+            id: 37,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'blue',
+            num: 11,
+            prev: 37,
+            id: 38,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'blue',
+            num: 10,
+            prev: 38,
+            id: 39,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'blue',
+            num: 9,
+            prev: 39,
+            id: 40,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'blue',
+            num: 8,
+            prev: 40,
+            id: 41,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'blue',
+            num: 7,
+            prev: 41,
+            id: 42,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'blue',
+            num: 6,
+            prev: 42,
+            id: 43,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'blue',
+            num: 5,
+            prev: 43,
+            id: 44,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'blue',
+            num: 4,
+            prev: 44,
+            id: 45,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'blue',
+            num: 3,
+            prev: 45,
+            id: 46,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'blue',
+            num: 2,
+            prev: 46,
+            id: 47,
+            selected: false,
+            disabled: false
+        },
+        {
+            color: 'blue',
+            num: <Lock />,
+            prev: 47,
+            id: 48,
+            selected: false,
+            disabled: false
+        }
+    ]
+
+export {
+    main
+}
---
+++ src/components/quixx/markers.tsx
@@ -0,0 +1,11 @@
+import { RocketLaunchIcon } from '@heroicons/react/24/solid';
+
+const ViewMarkers = () => {
+  return(
+    <div>
+      <RocketLaunchIcon />
+    </div>
+  );
+};
+
+export default ViewMarkers;
