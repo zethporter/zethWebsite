1ddddf7cdf7c0aa3ab6923191e21f400e00efd96
Zeth <zeth@bmisw.com>
2023-01-02 20:35:24 -0700

    updated some minor errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,9 +16,9 @@ const Home: NextPage = () => {
             <span className="text-cyan-300">Zeth</span> <span className="text-purple-300">Porter</span>
           </h1>
           <div className="container max-w-xs rounded-xl bg-white/10 p-4 text-white">
-            This is still under development. Check out some other stuff I've made below.
+            This is still under development. Check out some other stuff I&apos;ve made below.
           </div>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-1 md:gap-8">
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="/quixx"
